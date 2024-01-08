da468006cced3a0dfee4ed1101b04f9bc0299244
Rameshkumar1803 <139784575+Rameshkumar1803@users.noreply.github.com>
2024-01-08 10:20:23 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 # Simple Snake Game in Python 3 for Beginners
-# By @TokyoEdTech
+# By @RameshKumar
 
 import turtle
 import time
@@ -13,7 +13,7 @@ high_score = 0
 
 # Set up the screen
 wn = turtle.Screen()
-wn.title("Snake Game by @TokyoEdTech")
+wn.title("Snake Game by @RameshKumar")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
 wn.tracer(0) # Turns off the screen updates
